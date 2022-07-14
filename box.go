3c4bc205b43695af8d80328457081de96b65c1ac
Nezumi66 <nodirmirzo.tashniyazov@billz.uz>
2022-07-14 17:56:49 +0400

    Fixed Bug with appending addresses instead values

--- box.go
+++ box.go
@@ -90,7 +90,7 @@ func (b *box) RemoveAllCircles() error {
 
 	for _, shape := range b.shapes {
 		switch shape.(type) {
-		case Circle:
+		case *Circle:
 			continue
 		default:
 			shapes = append(shapes, shape)
